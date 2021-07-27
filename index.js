195a9894a0dd6fba342e15397d9b3d0196f65047
BarrelBrenner <rondolph@aol.com>
2021-07-26 20:42:02 -0500

    adjusted variables one

---
+++ index.js
@@ -0,0 +1,290 @@
+//Here are list of const packages required
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const consoleTable = require("console.table");
+
+//This let statement connects with mySQL database for it to recognize & read.
+let connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "IsabellaWilLin12th$",
+  database: "employee_tracker",
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+
+  runSearch();
+});
+
+//This function initiates inquirer to start prompts in list format
+function runSearch() {
+  inquirer.prompt({
+      name: "selection",
+      type: "list",
+      message: "Hello & Welcome to the Employee Tracker Database. Please select an option from list below.",
+      choices: [
+        "List Of All Employees",
+        "Observe Division",
+        "Observe Occupation",
+        "New Member",
+        "New Division",
+        "New Occupation",
+        "Adjust Occupation",
+      ],
+    })
+    .then(function (answer) {
+      console.log(answer);
+
+      if (answer.selection === "List Of All Employees") {
+        teamRoster();
+      } else if (answer.selection === "Observe Division") {
+        showSpecial();
+      } else if (answer.selection === "Observe Occupation") {
+        showAssign();
+      } else if (answer.selection === "New Member") {
+        plusRookie();
+      } else if (answer.selection === "New Division") {
+        plusSpecial();
+      } else if (answer.selection === "New Occupation") {
+        plusAssign();
+      } else if (answer.selection === "Adjust Occupation") {
+        designateAssign();
+      } else {
+        connection.end();
+      }
+    });
+}
+
+//This function allows user to view current employees in the database
+function teamRoster() {
+  connection.query(
+    "SELECT employee.id, employee.first_name, employee.last_name, employee.job_id, employee.leader_id, role.subtitle, role.rate, role.id, department.id FROM employee LEFT JOIN role ON employee.job_id = role.id LEFT JOIN department ON role.branch_id = department.id",
+    function (err, result, fields) {
+      if (err) throw err;
+      console.table(result);
+      runSearch();
+    }
+  );
+}
+
+//This function allows user to view assigned role of employee(s)
+function showAssign() {
+  connection.query(
+    "SELECT role.id, role.subtitle, role.rate, role.branch_id, department.id, department.name FROM role LEFT JOIN department on role.branch_id = department.id",
+    function (err, result, fields) {
+      if (err) throw err;
+      console.table(result);
+      runSearch();
+    }
+  );
+}
+
+//This function shows user which department employee(s) work in.
+function showSpecial() {
+  connection.query("SELECT * FROM department", function (err, result, fields) {
+    if (err) throw err;
+    console.table(result);
+    runSearch();
+  });
+}
+
+//These let statements fill out roles, employees & department for new/existing members.
+let jobSelect = [];
+let memberSelect = [];
+let divisionSelect = [];
+
+function researchJob() {
+  connection.query("SELECT * FROM role", function (err, data) {
+    if (err) throw err;
+    for (i = 0; i < data.length; i++) {
+      jobSelect.push(data[i].id + "-" + data[i].subtitle);
+    }
+  });
+}
+
+function researchMember() {
+  connection.query("SELECT * FROM employee", function (err, data) {
+    if (err) throw err;
+    for (i = 0; i < data.length; i++) {
+      memberSelect.push(data[i].id + "-" + data[i].first_name + " " + data[i].last_name);
+    }
+  });
+}
+
+function researchDivision() {
+  connection.query("SELECT * FROM department", function (err, data) {
+    if (err) throw err;
+    for (i = 0; i < data.length; i++) {
+      divisionSelect.push(data[i].id + "-" + data[i].name);
+    }
+  });
+}
+
+function plusRookie() {
+  researchJob();
+  researchMember();
+
+  inquirer.prompt([
+      {
+        name: "first",
+        type: "input",
+        message: "Please type in employee's first name.",
+      },
+
+      {
+        name: "last",
+        type: "input",
+        message: "Please type in employee's last name.",
+      },
+
+      {
+        name: "job",
+        type: "list",
+        message: "Please select employee's role from choices below.",
+        choices: jobSelect,
+      },
+
+      {
+        name: "designateFor",
+        type: "list",
+        message: "Please select employee's manager from choices below.",
+        choices: memberSelect,
+      },
+    ])
+    .then(function (answer) {
+      let getJobId = answer.job.split("-");
+      let getLeaderId = answer.designateFor.split("-");
+      let query = `INSERT INTO employee (first_name, last_name, job_id, leader_id)
+     VALUES ('${answer.first}','${answer.last}','${getJobId[0]}','${getLeaderId[0]}')`;
+      connection.query(query, function (err, res) {
+        console.log(
+          `Employee ${answer.first} ${answer.last} successfully joined the team!`
+        );
+      });
+      runSearch();
+    });
+}
+
+function plusAssign() {
+  researchJob();
+  researchMember();
+  researchDivision();
+
+  inquirer.prompt([
+      {
+        name: "job",
+        type: "input",
+        message: "Please type in role you want to see added:",
+      },
+
+      {
+        name: "branch",
+        type: "list",
+        message: "Please select department that you would add this role into.",
+        choices: divisionSelect,
+      },
+
+      {
+        name: "rate",
+        type: "number",
+        message: "Please type in salary rate for role in question:",
+      },
+    ])
+    .then(function (answer) {
+      console.log(`${answer.job}`);
+      let getBranchId = answer.branch.split("-");
+      let query = `INSERT INTO role (subtitle, rate, branch_id)
+   VALUES ('${answer.job}','${answer.rate}','${getBranchId[0]}')`;
+      connection.query(query, function (err, res) {
+        console.log(`<br>-----new role of ${answer.job} successfully added!------`);
+      });
+      runSearch();
+    });
+}
+
+function plusSpecial() {
+  researchJob();
+  researchMember();
+  researchDivision();
+
+  inquirer.prompt([
+      {
+        name: "branch",
+        type: "input",
+        message: "Please type in department you would like to see added:",
+      },
+    ])
+    .then(function (answer) {
+      let query = `INSERT INTO department (name)
+   VALUES ('${answer.branch}')`;
+      connection.query(query, function (err, res) {
+        console.log(`-------the following department has been successfully added: ${answer.branch}-------`);
+      });
+      runSearch();
+    });
+}
+
+function designateAssign() {
+  connection.query("SELECT * FROM employee", function (err, result) {
+    if (err) throw err;
+    inquirer.prompt([
+        {
+          name: "waiverClaim",
+          type: "list",
+
+          message: "Please select the following employees that are changing their roles.",
+          choices: function () {
+            let rosterArray = [];
+            result.forEach((result) => {
+              rosterArray.push(result.last_name);
+            });
+            return rosterArray;
+          },
+        },
+      ])
+
+      .then(function (answer) {
+        console.log(answer);
+        const name = answer.waiverClaim;
+
+        connection.query("SELECT * FROM role", function (err, res) {
+          inquirer.prompt([
+              {
+                name: "job",
+                type: "list",
+                message: "Please select what their new role will become.",
+                choices: function () {
+                  let roleArray = [];
+                  res.forEach((res) => {
+                    roleArray.push(res.title);
+                  });
+                  return roleArray;
+                },
+              },
+            ])
+            .then(function (roleAnswer) {
+              const role = roleAnswer.role;
+              console.log(role);
+              connection.query(
+                "SELECT * FROM role WHERE subtitle = ?",
+                [role],
+                function (err, res) {
+                  if (err) throw err;
+                  let jobId = res[0].id;
+
+                  let query = "UPDATE employee SET job_id = ? WHERE last_name =  ?";
+                  let values = [parseInt(jobId), name];
+
+                  connection.query(query, values, function (err, res, fields) {
+                    console.log(`Success! Updated ${name}'s role to ${role}.`);
+                  });
+                  teamRoster();
+                }
+              );
+            });
+        });
+      });
+  });
+}
---
+++ schema.sql
@@ -0,0 +1,55 @@
+DROP DATABASE IF EXISTS employee_tracker;
+CREATE DATABASE employee_tracker;
+
+USE employee_tracker;
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  job_id INT,
+  leader_id INT,
+  PRIMARY KEY (id),
+    -- FOREIGN KEY(job_id)
+    -- REFERENCES role(id),
+    FOREIGN KEY(leader_id)
+    REFERENCES employee(id)
+
+);
+INSERT INTO employee(first_name, last_name, job_id, leader_id) VALUES ("Brenner", "Wolf", 1, 1);
+INSERT INTO employee(first_name, last_name, job_id, leader_id) VALUES ("Raymond", "Beasley", 2, 1);
+INSERT INTO employee(first_name, last_name, job_id, leader_id) VALUES ("Isaac", "Caulder", 4, 1);
+INSERT INTO employee(first_name, last_name, job_id, leader_id) VALUES ("Otto", "Morris", 3, 1);
+INSERT INTO employee(first_name, last_name, job_id, leader_id) VALUES ("Forsythe", "Knight", 5, 1);
+INSERT INTO employee(first_name, last_name, job_id, leader_id) VALUES ("Gage", "Sharp", 2, 1);
+INSERT INTO employee(first_name, last_name, job_id, leader_id) VALUES ("Sasha", "Bird", 3, 1);
+INSERT INTO employee(first_name, last_name, job_id, leader_id) VALUES ("Waylon", "Brothers", 1, 1 );
+INSERT INTO employee(first_name, last_name, job_id, leader_id) VALUES ("Ruben", "Greyfield", 1, 1);
+
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  subtitle VARCHAR(30) NOT NULL,
+  rate DECIMAL NOT NULL,
+  branch_id INT,
+  PRIMARY KEY (id)
+  -- FOREIGN KEY(branch_id)
+  -- REFERENCES department(id)
+);
+insert into role(subtitle, rate, branch_id) values('Scientist', 5000000, 1);
+insert into role(subtitle, rate, branch_id) values('Guard', 1000000, 2);
+insert into role(subtitle, rate, branch_id) values('Trader', 4000000, 3);
+insert into role(subtitle, rate, branch_id) values('Technician', 3000000, 4);
+insert into role(subtitle, rate, branch_id) values('Boss', 2000000, 5);
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+insert into department(name)
+values ('Research'),
+       ('Security'),
+       ('Finance'),
+       ('Technology'),
+       ('Project Lead');
